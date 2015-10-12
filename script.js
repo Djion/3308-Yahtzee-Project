431a544bcb8228e033d59cdaea31c51658a0390c
Djion <alri7215@colorado.edu>
2015-10-12 13:39:42 -0600

    Added the ability to select aces as a score selection

--- script.js
+++ script.js
@@ -11,18 +11,13 @@ var dice5;
 $(document).ready(function() {
     c = $("#yahtzee")[0];
     ctx = c.getContext("2d");
+    ctx.font = "30px Arial";
     diceImg = new Image();
     diceImg.src = 'assets/dice.png'
 
     $("#rollButton").click(function() {
         playGame();
     });
-
-    $(document.body).on('click', '.dropdown-menu li a', function (e) {
-    var selText = $(this).text(); 
-    scoreSelection(selText);
-    });
-
 });
 
 //Returns a number from 1-6
@@ -49,11 +44,11 @@ var playGame = function(){
     clearScreen();
 
     //Setting the dice
-    var dice1 = diceRoll();
-    var dice2 = diceRoll();
-    var dice3 = diceRoll();
-    var dice4 = diceRoll();
-    var dice5 = diceRoll();
+    dice1 = diceRoll();
+    dice2 = diceRoll();
+    dice3 = diceRoll();
+    dice4 = diceRoll();
+    dice5 = diceRoll();
 
     var diceHeight = c.height * 0.1;
 
@@ -64,11 +59,33 @@ var playGame = function(){
     drawDice(dice4, 64 * 4, diceHeight);
     drawDice(dice5, 64 * 5, diceHeight);
     console.log(dice1 + ", " + dice2 + ", " + dice3 + ", " + dice4 + ", " + dice5);
+
+    $(document.body).on('click', '.dropdown-menu li a', function (e) {
+    var selText = $(this).text(); 
+    scoreSelection(selText,dice1,dice2,dice3,dice4,dice5);
+    });
 }
 
-var scoreSelection = function(selText){
+var scoreSelection = function(selText,d1,d2,d3,d4,d5){
     console.log(selText);
-    
+    if(selText === "Aces")
+    {
+        var scoreText = ace_score(d1,d2,d3,d4,d5);
+        console.log(scoreText);
+        ctx.fillText("Score for Aces is " + scoreText,30,150);
+
+    }
 }
 
+var ace_score = function(d1,d2,d3,d4,d5)
+{
+    var total_score = 0;
+    var diceArray = [d1,d2,d3,d4,d5];
+    for(i = 0; i < 5; i++){
+        if(diceArray[i] === 1){
+            total_score = total_score +1;
+        }
+    }
+    return total_score;
+}
 
