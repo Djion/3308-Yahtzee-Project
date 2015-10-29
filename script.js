cb3a67251e4290ce35625e43a784753e977d9cd2
Robert Allen <robertcallen585@gmail.com>
2015-10-28 23:30:22 -0600

    Created a dice class to simplify work as well as reverted to single image for dice

--- script.js
+++ script.js
@@ -1,61 +1,100 @@
 var c;
 var ctx;
+
+var allDice = [];
 var diceImg;
 
+// class Dice
+var Dice = function(diceNum, isActive) {
+    this.diceNum = diceNum;
+    this.diceVal = Math.floor(Math.random() * 6) + 1;
+    this.isActive = isActive;
+    this.dimen = 48;
+}
 
-var dice1;
-var dice2;
-var dice3;
-var dice4;
-var dice5;
-var activeDice = [dice1, dice2, dice3, dice4, dice5];
-var diceImgs = [];
+// Creates a function on all dice objects called diceRoll
+Dice.prototype.diceRoll = function() {
+    this.diceVal = Math.floor(Math.random() * 6) + 1;
+}
 
-//Setup the game
+// Will give the coordinate box of a dice on the canvas
+Dice.prototype.loc = function() {
+    var loc = {};
+    loc['startX'] = 16 + 64 * (this.diceNum - 1);
+    loc['startY'] = (this.isActive) ? 16 : 80;
+    loc['endX'] = loc['startX'] + this.diceDimen;
+    loc['endY'] = loc['startY'] + this.diceDimen;
+
+    return loc;
+}
+
+// Draws a dice at the correct location with the correct image
+Dice.prototype.draw = function() {
+    var imgDiceDimen = 128;
+    loc = this.loc();
+    ctx.drawImage(diceImg, (this.diceVal - 1) * imgDiceDimen, 0, imgDiceDimen, imgDiceDimen, loc['startX'], loc['startY'], this.dimen, this.dimen);
+}
+
+// Setup the game
 $(document).ready(function() {
     c = $("#yahtzee")[0];
     ctx = c.getContext("2d");
     ctx.font = "30px Arial";
+
     diceImg = new Image();
     diceImg.src = 'assets/dice.png';
 
-    
+    for (var i = 1; i <= 5; ++i)
+        allDice.push(new Dice(i, true))
 
     $("#rollButton").click(function() {
         playGame();
     });
 
-    $(document.body).on('click', '.dropdown-menu li a', function (e) {
+    $('.dropdown-menu li a').click(function() {
         var selText = $(this).text(); 
         scoreSelection(selText);
     });
 });
-
-//Returns a number from 1-6
-var diceRoll = function() {
-    return Math.floor(Math.random() * 6 + 1);
-}
 
 // Clears the canvas for redrawing
 var clearScreen = function() {
     ctx.clearRect(0, 0, c.width, c.height);
 }
 
-//Draws diceNum from the dice.png image to the screen at (posX, posY)
-var drawDice = function(diceNum, posX, posY) {
-    var diceHeightWidth = 128;
-
-    var diceStartX = (diceNum - 1) * 128;
-    var diceEndX = diceStartX + 128;
-
-    ctx.drawImage(diceImg, diceStartX, 0, diceHeightWidth, diceHeightWidth, posX, posY, 48, 48);
+// Draws image associated with diceVal at location give by diceNum
+var drawDice = function() {
+    for (var i = 0; i < allDice.length; ++i)
+        allDice[i].draw();
 }
 
 
 var rollActiveDice = function() {
-    for (var i = 0; i < activeDice.length; ++i) {
-        activeDice[i] = diceRoll(activeDice[i]);
+    for (var i = 0; i < allDice.length; ++i)
+        if (allDice[i].isActive)
+            allDice[i].diceRoll();
+}
+
+var scoreSelection = function(selText){
+    console.log(selText);
+    if (selText === "Aces") {
+        var scoreText = oneToSixSum(1);
+        console.log(scoreText);
+        ctx.fillText("Score for Aces is " + scoreText, 30, 150);
     }
+}
+
+var oneToSixSum = function(c)
+{
+    var total_score = 0;
+
+    for (i = 0; i < 5; i++) {
+        if (allDice[i].diceVal === c){
+            total_score += c;
+        }
+    }
+
+    return total_score;
 }
 
 var playGame = function(){
@@ -65,39 +104,5 @@ var playGame = function(){
     rollActiveDice();
 
     //Disply the rolled dice to the screen
-
-    for (var i = 0; i < activeDice.length; ++i){
-        drawDice(activeDice[i], 64 * (i + 1), c.height * 0.1);
-    }
-
-    console.log(dice1 + ", " + dice2 + ", " + dice3 + ", " + dice4 + ", " + dice5);
-
-    $(document.body).on('click', '.dropdown-menu li a', function (e) {
-        var selText = $(this).text(); 
-        scoreSelection(selText,dice1,dice2,dice3,dice4,dice5);
-    });
+    drawDice();
 }
-
-var scoreSelection = function(selText,d1,d2,d3,d4,d5){
-    console.log(selText);
-    if(selText === "Aces")
-    {
-        var scoreText = OneToSix_Sum(1,d1,d2,d3,d4,d5);
-        console.log(scoreText);
-        ctx.fillText("Score for Aces is " + scoreText,30,150);
-
-    }
-}
-
-var OneToSix_Sum = function(c,d1,d2,d3,d4,d5)
-{
-    var total_score = 0;
-    var diceArray = [d1,d2,d3,d4,d5];
-    for(i = 0; i < 5; i++){
-        if(diceArray[i] === c){
-            total_score = total_score +c;
-        }
-    }
-    return total_score;
-}
-
