085b97c11ac0e2bb58e746b22114a826fc378fc3
Robert Allen <robert.c.allen@colorado.edu>
2015-10-28 21:44:39 -0600

    Started work on the pick dice function

--- script.js
+++ script.js
@@ -1,26 +1,32 @@
 var c;
 var ctx;
 var diceImg;
+
+
 var dice1;
 var dice2;
 var dice3;
 var dice4;
 var dice5;
+var activeDice = [dice1, dice2, dice3, dice4];
+var diceImgs = [];
 
 //Setup the game
 $(document).ready(function() {
     c = $("#yahtzee")[0];
     ctx = c.getContext("2d");
     diceImg = new Image();
-    diceImg.src = 'assets/dice.png'
+    diceImg.src = 'assets/dice.png';
+
+    
 
     $("#rollButton").click(function() {
         playGame();
     });
 
     $(document.body).on('click', '.dropdown-menu li a', function (e) {
-    var selText = $(this).text(); 
-    scoreSelection(selText);
+        var selText = $(this).text(); 
+        scoreSelection(selText);
     });
 
 });
@@ -36,7 +42,7 @@ var clearScreen = function() {
 }
 
 //Draws diceNum from the dice.png image to the screen at (posX, posY)
-function drawDice(diceNum, posX, posY) {
+var drawDice = function(diceNum, posX, posY) {
     var diceHeightWidth = 128;
 
     var diceStartX = (diceNum - 1) * 128;
@@ -45,24 +51,25 @@ function drawDice(diceNum, posX, posY) {
     ctx.drawImage(diceImg, diceStartX, 0, diceHeightWidth, diceHeightWidth, posX, posY, 48, 48);
 }
 
+
+var rollActiveDice = function() {
+    for (var i = 0; i < activeDice.length; ++i) {
+        activeDice[i] = diceRoll(activeDice[i]);
+    }
+}
+
 var playGame = function(){
     clearScreen();
 
     //Setting the dice
-    var dice1 = diceRoll();
-    var dice2 = diceRoll();
-    var dice3 = diceRoll();
-    var dice4 = diceRoll();
-    var dice5 = diceRoll();
-
-    var diceHeight = c.height * 0.1;
+    rollActiveDice();
 
     //Disply the rolled dice to the screen
-    drawDice(dice1, 64, diceHeight)
-    drawDice(dice2, 64 * 2, diceHeight);
-    drawDice(dice3, 64 * 3, diceHeight);
-    drawDice(dice4, 64 * 4, diceHeight);
-    drawDice(dice5, 64 * 5, diceHeight);
+
+    for (var i = 0; i < activeDice.length; ++i){
+        drawDice(activeDice[i], 64 * (i + 1), c.height * 0.1);
+    }
+
     console.log(dice1 + ", " + dice2 + ", " + dice3 + ", " + dice4 + ", " + dice5);
 }
 
