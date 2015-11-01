a10bb79b74d5eb640253aad49a96815ce18e9015
David-Zhuzhunashvili <david.zhuzhunashvili@colorado.edu>
2015-11-01 14:28:42 -0700

    Changed all the methods to increase performance and fixed errors that would cause problems for sure like using multiple inequalities without &&.

--- OneToSix_Sum.js
+++ OneToSix_Sum.js
@@ -65,8 +65,8 @@ var Large_Straight = function(Dice)
 	}else if (numbers[1] == 1 && numbers[2] == 1 && numbers[3] == 1 && numbers[4] == 1 && numbers[5] == 1) {
 		total_score = 20
 	}
+
 	return total_score;
-	
 }
 
 //Function to return the scorecard option for Full House (A full house is when you have three of one number and 2 of another number example : 2,2,3,3,3)
@@ -109,22 +109,20 @@ var Full_House = function(Dice)
 var Three_OAK = function(Dice)
 {
 	var total_score = 0;
-	//This array is to store the number of dice that are the same. If the passed array is 1,1,2,2,4 the numbers array will read 2,2,0,1,0
-	var numbers = [0,0,0,0,0];
-	// Creates the values for the Numbers array explained above
-	for(i = 0; i < 5; i++){
-		for(k = 0; k < 5; k++){
-			if(i+1 === Dice[k]){
-				numbers[i] = numbers[i]++;
-			}
-		}
+	//The numbers variable stores the number of a certain die we have, for example if we had 2 1's it would look like [2,0,0,0,0,0]
+	var numbers = [0, 0, 0, 0, 0, 0];
+
+	//Increments the numbers array by one at the index of the die
+	for (var i = 0; i < 5; i++) {
+		numbers[Dice[i] - 1]++;
 	}
+
 	// Looks at the array of numbers for a 3 of a kind if it finds one sets the total score to 3 of that value
-	for(i = 0; i < 5; i++)
+	for(i = 0; i < 6; i++)
 	{
-		if(numbers[i] === 3){
-			total_score = i * 3;
-			break;
+		if(numbers[i] >= 3){
+			total_score = 3*(i+1);
+			return total_score;
 		}
 	}
 
@@ -137,24 +135,20 @@ var Three_OAK = function(Dice)
 var Four_OAK = function(Dice)
 {
 	var total_score = 0;
-	//This array is to store the number of dice that are the same. If the passed array is 1,1,2,2,4 the numbers array will read 2,2,0,1,0
-	var numbers = [0,0,0,0,0];
+	//The numbers variable stores the number of a certain die we have, for example if we had 2 1's it would look like [2,0,0,0,0,0]
+	var numbers = [0, 0, 0, 0, 0, 0];
 
-
-	// Creates the values for the Numbers array explained above
-	for(i = 0; i < 5; i++){
-		for(k = 0; k < 5; k++){
-			if(i+1 === Dice[k]){
-				numbers[i] = numbers[i]++;
-			}
-		}
+	//Increments the numbers array by one at the index of the die
+	for (var i = 0; i < 5; i++) {
+		numbers[Dice[i] - 1]++;
 	}
+
 	// Looks at the array of numbers for a 4 of a kind if it finds one sets the total score to 4 of that value
-	for(i = 0; i < 5; i++)
+	for(i = 0; i < 6; i++)
 	{
-		if(numbers[i] === 4){
-			total_score = i * 4;
-			break;
+		if(numbers[i] >= 4){
+			total_score = 4*(i+1);
+			return total_score;
 		}
 	}
 
@@ -166,11 +160,12 @@ var Four_OAK = function(Dice)
 var Five_OAK = function(Dice)
 {
 	var total_score = 0;
-	if(Dice[0] === Dice[1] === Dice[2] === Dice[3] === Dice[4]){
+
+	//Checks if 5 dice are equal
+	if(Dice[0] === Dice[1] && Dice[0] === Dice[2] && Dice[0]=== Dice[3] && Dice[0]=== Dice[4]){
 		total_score = Dice[0] * 5;
 		return total_score;
 	}
-	else{
-	return total_score;
-	}
+
+	return total_score;	
 }
